b7bb5fc90cfd7a3bc34f8e9cae1a5e6522c9921e
Ryan Collanton <rpcollanton@gmail.com>
2023-01-20 12:36:08 -0600

    Separate binned bonds into its own class. Account for PBCs when binning. Compute method in progress.

--- polymerMD/simtools/ik.py
+++ polymerMD/simtools/ik.py
@@ -61,7 +61,7 @@ class ThermoIKUpdater(hoomd.custom.Action):
                 self._thermoIK.BinnedNListPairs.updateBinnedLists(snap.particles.position, nlist_arrays)
 
             # update bond neighbor lists
-            self._thermoIK.BinnedNListBonds.updateBinnedLists(snap.particles.position, snap.bonds.group)
+            self._thermoIK.BinnedBonds.updateBondLists(snap.particles.position, snap.bonds.group)
 
             # re-compute spatial thermo
             self._thermoIK.compute()
@@ -79,7 +79,7 @@ class ThermoIK(metaclass=hoomd.logging.Loggable):
 
         # create binned neighbor list objects to be updated later
         self.BinnedNListPairs = BinnedNeighborLists(nbins, axis, self._sim.state.box)
-        self.BinnedNListBonds = BinnedNeighborLists(nbins, axis, self._sim.state.box)
+        self.BinnedBonds = BinnedBonds(nbins, axis, self._sim.state.box)
 
         # create thermoIKUpdater, passing this object into it. assume pair force is the second element
         # This will be an action added to a custom writer and added to sim.operations.writers, see ik.add_thermo_ik
@@ -95,18 +95,41 @@ class ThermoIK(metaclass=hoomd.logging.Loggable):
     
     def _pre_compute(self):
         self._divA = self._sim.state.box.L[self._axis]/np.product(self._sim.state.box.L)
+        self._divbinVol = 1/np.product(self._sim.state.box.L)*self._nbins
         return
     
     def compute(self):
+        # reset spatial pressure tensor
+        self._spatial_pressure_tensor = np.zeros((self._nbins,6))
 
         # get kT from the simulation's current state
         kT = self._sim.operations.computes[0].kinetic_temperature
 
         # virial contribution from pair interaction (add bond later) for each bin
         for i in range(self.BinnedNListPairs._nbins):
-            vir = self._divA * self._sim.operations.integrator.forces[1].compute_virial_pressure_contribution(self.BinnedNListPairs._nLists[i], self._axis)
-            self._spatial_pressure_tensor[i,:] += vir
+            # Kinetic contribution to the pressure, proportional to temperature and local density
+            numdens = self._divbinVol*self.BinnedNListPairs._bincount[i] # maybe gaussian smear this later?
+            kinetic_p = kT*numdens # need to ensemble average this separately!!!?
+            self._spatial_pressure_tensor[i,0] += kinetic_p
+            self._spatial_pressure_tensor[i,3] += kinetic_p
+            self._spatial_pressure_tensor[i,5] += kinetic_p
 
+            edge0 = self.BinnedNListPairs.edges[i]
+            edge1 = self.BinnedNListPairs.edges[i+1]
+
+            # Bond and pair virial contributions to the pressure
+            virPair = self._divA*self._sim.operations.integrator.forces[1].compute_virial_pressure_contribution(self.BinnedNListPairs.nlists[i], self._axis, edge0, edge1)
+            virBond = self._divA*self._sim.operations.integrator.forces[0].compute_virial_pressure_contribution(self.BinnedBonds.bondlists[i], self._axis, edge0, edge1)
+            # print("virPair: " + str(virPair))
+            # print("virBond: " + str(virBond))
+
+            self._spatial_pressure_tensor[i,:] += self._divA * (virPair + virBond)
+            #print(self._spatial_pressure_tensor)
+
+        # print averaged from my calculation at this frame
+        print("Average IK: ", 1/self._nbins*np.sum(self._spatial_pressure_tensor,axis=0))
+        print("HOOMD Result: ", self._sim.operations.computes[0].pressure_tensor)
+        
         # smeared average density function from freud, to be evaluated at each box's midpoint
         # gaussian density?
 
@@ -161,6 +184,66 @@ class nListStager:
         self._distances.append(distance)
         return
 
+class BinnedBonds:
+
+    def __init__(self, nBins: int, axis: int, box: hoomd.Box):
+        # nunber of bins and dimension along which bins are created
+        self._nbins = nBins
+        self._axis = axis
+
+        # get dimensions of axis
+        self._L = box.L[axis]
+
+        # bin edge points
+        self._edges = np.linspace(-self._L/2,+self._L/2,self._nbins+1)
+
+        # create list of member and neighbor lists for each bin
+        self._bondLists = [[] for i in range(self._nbins)] # populate with a list of bond tags for each bin
+        return
+
+    def updateBondLists(self, positions: np.ndarray, bonds: hoomd.data.array):
+
+        # reset stored bond arrays
+        self._bondLists = [[] for i in range(self._nbins)]
+
+        # determine the bin each particle belongs to. Note, first bin has index 1 from np.digitize
+        particle_bins = np.digitize(positions[:,self._axis], self._edges)
+
+        # loop over bonds
+        for bondtag,bondedpair in enumerate(bonds):
+            # indices of particles participating in this bond
+            i = bondedpair[0]
+            j = bondedpair[1]
+            # indices of bins that these two particles are in
+            bin_i = particle_bins[i]-1
+            bin_j = particle_bins[j]-1
+            # add pair to each bin between the two bins
+            for bin_idx in self._getBinsBetween(bin_i, bin_j, self._nbins):
+                self._bondLists[bin_idx].append(bondtag)
+
+        return
+    
+    def _getBinsBetween(self, i: int, j: int, n: int):
+        # i and j are bin indices, could be greater or lesser, or same
+        # n is the number of bins
+        l = min((i,j)) # lower bin
+        u = max((i,j)) # upper bin
+        if l == u:
+            binsbtwn = [l]
+        elif (u-l) < (l+n-u):
+            binsbtwn = list(range(l,u+1))
+        else:
+            binsbtwn = list(range(u,n)) + list(range(0,l+1))
+        return binsbtwn
+    
+    @property 
+    def bondlists(self):
+        return self._bondLists
+    
+    @property
+    def edges(self):
+        return self._edges
+    
 class BinnedNeighborLists:
 
     def __init__(self, nBins: int, axis: int, box: hoomd.Box): 
@@ -174,8 +257,10 @@ class BinnedNeighborLists:
         # bin edge points
         self._edges = np.linspace(-self._L/2,+self._L/2,self._nbins+1)
 
+        # number of particles physically in each bin
+        self._bincount = np.zeros((self._nbins))
+
         # create list of member and neighbor lists for each bin
-        self._members = [[] for i in range(nBins)]
         self._nListStagers = [nListStager() for i in range(nBins)] 
         self._nLists = [] # created once neighbor arrays are populated
         return
@@ -186,8 +271,6 @@ class BinnedNeighborLists:
             self._updateLists_hoomd(positions, nlist)
         elif isinstance(nlist, freud.locality.NeighborList):
             self._updateLists_freud(positions, nlist)
-        else: # assume it is an array. recall error with using isinstance with List[List[int]] "subscripting of generics"
-            self._updateLists_array(positions, nlist)
         return
 
     def _updateLists_hoomd(self, positions: np.ndarray, nlist_arrays: hoomd.md.data.NeighborListLocalAccess):
@@ -205,6 +288,7 @@ class BinnedNeighborLists:
         
         # determine the bin each particle belongs to. Note, first bin has index 1 from np.digitize
         particle_bins = np.digitize(positions[:,self._axis], self._edges)
+        self._bincount = np.histogram(positions[:,self._axis], bins=self._edges, density=False)[0]
 
         # This loops over all particles and then each of their neighbors, and determines
         # what bin(s) the interaction of each pair contributes to 
@@ -237,40 +321,15 @@ class BinnedNeighborLists:
     def _updateLists_freud(self, positions: np.ndarray, nlist: freud.locality.NeighborList):
         # TO BE WRITTEN! don't need it yet
         return
-
-    def _updateLists_array(self, positions: np.ndarray, bonds: hoomd.data.array):
-
-        # reset stored neighbor arrays
-        for stager in self._nListStagers:
-            stager.reset()
-
-        # determine the bin each particle belongs to. Note, first bin has index 1 from np.digitize
-        particle_bins = np.digitize(positions[:,self._axis], self._edges)
-
-        # loop over bonds
-        for bondedpair in bonds:
-            # indices of particles participating in this bond
-            i = bondedpair[0]
-            j = bondedpair[1]
-            # indices of bins that these two particles are in
-            bin_i = particle_bins[i]-1
-            bin_j = particle_bins[j]-1
-            dist_ij = np.sqrt(np.sum(np.square(positions[j,:]-positions[i,:])))
-            # add pair to each bin between the two bins
-            for bin_idx in self._getBinsBetween(bin_i, bin_j, self._nbins):
-                self._nListStagers[bin_idx].add_pair((i,j), dist_ij)
-        
-        # after adding all pairs, build neighbor lists
-        self._nLists = [stager.build_nlist(len(positions)) for stager in self._nListStagers]
-
-        return
     
     def _getBinsBetween(self, i: int, j: int, n: int):
-        # i and j are bin indices, could be greater or lesser
+        # i and j are bin indices, could be greater or lesser, or same
         # n is the number of bins
         l = min((i,j)) # lower bin
         u = max((i,j)) # upper bin
-        if (u-l) < (l+n-u):
+        if l == u:
+            binsbtwn = [l]
+        elif (u-l) < (l+n-u):
             binsbtwn = list(range(l,u+1))
         else:
             binsbtwn = list(range(u,n)) + list(range(0,l+1))
